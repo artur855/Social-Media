028f15c7fb2a22d3c12d1cba477bbaf962538d50
artur855 <artur855@hotmail.com>
2018-01-31 22:06:24 -0300

    Add tag and commit repository

---
+++ src/main/java/com/arthurzera/website/repositories/CommentRepository.java
@@ -0,0 +1,16 @@
+package com.arthurzera.website.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.arthurzera.website.models.Comment;
+import com.arthurzera.website.models.Post;
+import com.arthurzera.website.models.User;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+	Comment findByUser(User user);
+	Comment findByPost(Post post);
+	Comment findByBody(String body);
+	
+}
---
+++ src/main/java/com/arthurzera/website/repositories/TagRepository.java
@@ -0,0 +1,12 @@
+package com.arthurzera.website.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.arthurzera.website.models.Tag;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, Long>{
+	Tag findByTag(String tag);
+	Tag findById(Long id);
+}
