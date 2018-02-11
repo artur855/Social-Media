be65577af5dfb098348222780ab47f0f6b1f3110
artur855 <artur855@hotmail.com>
2018-02-10 22:23:56 -0300

    Changed post design

--- src/main/java/com/arthurzera/social/media/SocialMediaApplication.java
+++ src/main/java/com/arthurzera/social/media/SocialMediaApplication.java
@@ -6,9 +6,9 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.arthurzera.social.media")
-public class WebsiteApplication {
+public class SocialMediaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebsiteApplication.class, args);
+		SpringApplication.run(SocialMediaApplication.class, args);
 	}
 }
